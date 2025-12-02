28333ede5eb9f11f8c76c4027246388c39e829e0
Kawan-idres <kawanedres211@gmail.com>
2025-12-02 16:37:43 +0300

    modules

--- rust-crash-course/src/main.rs
+++ rust-crash-course/src/main.rs
@@ -1,20 +1,9 @@
 // Enable strict linting - all clippy warnings become compilation errors
 #![deny(clippy::all)]
 // T must implement Display
-fn print_it<T: std::fmt::Display>(value: T) {
-    println!("Value: {}", value);
-}
-
-// T must implement Clone
-fn duplicate<T: Clone>(value: T) -> (T, T) {
-    (value.clone(), value)
-}
+mod math;  // Tells Rust to look for math.rs
 
 fn main() {
-    print_it(42);
-    print_it("hello");
-    
-    let s = String::from("hello");
-    let (s1, s2) = duplicate(&s);
-    println!("{}, {}, {}", s1, s2,s);
+    println!("2 + 3 = {}", math::add(2, 3));
+    println!("5 - 2 = {}", math::subtract(5, 2));
 }
